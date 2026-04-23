c9a0308d15a1e2e7a96b75a22bcaedb2009e4048
Noé Lopez <noelopez@free.fr>
2026-04-23 19:58:26 +0200

    gnu: Add libglycin.
    We split the glycin loaders from libglycin to avoid dependency circles between
    gdk-pixbuf and glycin:
    
    glycin-loaders depends transitively on gdk-pixbuf. And gdk-pixbuf must depend
    on glycin at some point, since the transition plan is that it starts using it
    as a backend instead of its builtin loaders.
    
    With this commit, gdk-pixbuf can depend on libglycin; avoiding the extra
    dependencies brought by the loaders.
    
    This split is also recommended in Glycin’s README.
    
    * gnu/packages/gnome.scm
    (libglycin): New variable.
    (glycin-loaders): Define as inherited package of libglycin.
    (loupe, nautilus)[inputs]: Add libglycin.
    
    Change-Id: I424b2227882b9ca1cbd9c5130ac7239c22d59e56

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -6987,9 +6987,9 @@ side panel;
     (license license:gpl2+)))
 
 ;; Also update (@ (gnu packages rust-sources) rust-glycin-3) when updating this.
-(define-public glycin-loaders
+(define-public libglycin
   (package
-    (name "glycin-loaders")
+    (name "libglycin")
     (version "2.1.1")
     (source
      (origin
@@ -7006,6 +7006,10 @@ side panel;
     (build-system meson-build-system)
     (arguments
      (list
+      #:configure-flags #~(list "-Dglycin-loaders=false"
+                                "-Dglycin-thumbnailer=true"
+                                "-Dlibglycin=true"
+                                "-Dlibglycin-gtk4=true")
       #:imported-modules `(,@%meson-build-system-modules
                            ,@%cargo-build-system-modules)
       #:modules `(((guix build cargo-build-system) #:prefix cargo:)
@@ -7097,9 +7101,6 @@ side panel;
                             (base32
                              "1jvra75kjqkndxzzpmi5lb8d3q4fi3yc4hha0r1g9brdsnbhsp6f")))))
     (inputs (cons* bubblewrap
-                   libheif
-                   libjxl
-                   librsvg
                    (cargo-inputs 'glycin)))
     ;; The following inputs are 'Required' in the pkg-config files.
     (propagated-inputs (list fontconfig glib gtk lcms libseccomp))
@@ -7109,6 +7110,22 @@ side panel;
 thumbnails to display untrusted content safely.  This package provides the
 runtime image loader executables that are used inside the sandbox.")
     (license (list license:mpl2.0 license:lgpl2.1+))))
+
+(define-public glycin-loaders
+  (package/inherit libglycin
+    (name "glycin-loaders")
+    (arguments
+     (substitute-keyword-arguments arguments
+       ((#:configure-flags original-flags #~(list))
+        #~(list "-Dglycin-loaders=true"
+                "-Dglycin-thumbnailer=false"
+                "-Dlibglycin=false"
+                "-Dlibglycin-gtk4=false"))))
+    (inputs
+     (modify-inputs inputs
+       (prepend libheif
+                libjxl
+                librsvg)))))
 
 (define-public loupe
   (package
@@ -7180,6 +7197,7 @@ runtime image loader executables that are used inside the sandbox.")
             gtk
             lcms
             libadwaita
+            libglycin
             libgweather
             libseccomp
             (cargo-inputs 'loupe)))
@@ -10221,6 +10239,7 @@ through portals.")
            gst-plugins-base
            json-glib
            libadwaita
+           libglycin
            libportal
            libseccomp
            libselinux
