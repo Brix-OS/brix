a491e3d9d6dfc83b26b01c97ef975354e93cd27b
Khalid Rafi <khaalidrafi@gmail.com>
2026-07-28 10:49:29 +0600

    BRIX: Add Nonguix module dirs to build guix-system.drv

--- guix/self.scm
+++ guix/self.scm
@@ -179,7 +179,7 @@ NODE's modules, under their FHS directories: share/guile/site and lib/guile."
                            (('guix 'config)
                             #f)
                            ((and module
-                                 (or ('guix _ ...) ('gnu _ ...)))
+                                 (or ('guix _ ...) ('gnu _ ...) ('nonguix _ ...) ('nongnu _ ...)))
                             (not (member module except)))
                            (rest #f))))
 
@@ -984,7 +984,12 @@ itself."
                    ,@(scheme-modules* source "gnu/system")
                    ,@(scheme-modules* source "gnu/services")
                    ,@(scheme-modules* source "gnu/machine")
-                   ,@(scheme-modules* source "guix/platforms/"))
+                   ,@(scheme-modules* source "guix/platforms/")
+
+                   ,@(scheme-modules* source "nongnu/packages")
+                   ,@(scheme-modules* source "nongnu/packages/linux")
+                   ,@(scheme-modules* source "nonguix/licenses")
+                   )
                  (list *core-package-modules* *package-modules*
                        *extra-modules* *core-modules*)
                  #:extensions dependencies
